77f55e09396b7ec994a64ce8d7c6e98d35626237
janainaacm <janaina@Janainas-Air.lan>
2023-09-20 11:12:51 +0200

    Uppgift 1

--- src/com/Janaina/demo/Main.java
+++ src/com/Janaina/demo/Main.java
@@ -1,6 +1,20 @@
 package com.Janaina.demo;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        //Uppgift 1
+        System.out.println("What's your name player?");
+        String playerOneName = sc.nextLine();
+
+        System.out.println("Player #1: " + playerOneName);
+
+
+    }
 
 }
