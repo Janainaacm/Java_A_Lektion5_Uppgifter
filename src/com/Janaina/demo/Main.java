6f71003f3ce73acd188308cdaa5e7893a8854569
janainaacm <janaina@Janainas-Air.lan>
2023-09-20 11:21:56 +0200

    Uppgift 4

--- src/com/Janaina/demo/Main.java
+++ src/com/Janaina/demo/Main.java
@@ -8,21 +8,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        //Uppgift 3
+        //Uppgift 4
         boolean isPlaying = true;
+
 
         do {
 
-            System.out.println("Say the magic word");
-            String magicWord = sc.nextLine();
+            System.out.println("Type 1 to play, and 'stop' to exit!");
+            String swstring = sc.nextLine();
 
-            if (magicWord.equals("stop")){
-                isPlaying = false;
+            switch(swstring){
+                case "stop":
+                    isPlaying = false;
+                    break;
+                case "1":
+                    System.out.println("Throwing!");
+                    break;
+                default:
+                    System.out.println("Wrong input");
+                    break;
             }
 
-
         } while (isPlaying);
-
 
     }
 
